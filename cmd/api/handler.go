55377c20d83a8c8316d68ec07a7693c6cefaef7d
Eric <eric.sanchez@onclusive.com>
2024-12-15 19:09:11 +0100

    fix: handler return on error

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -7,10 +7,13 @@ import (
 
 func (a *app) getProductsHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
+	
+	ctx := r.Context()
 
-	products, err := a.productService.GetProducts(r.Context())
+	products, err := a.productService.GetProducts(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	err = json.NewEncoder(w).Encode(products)
